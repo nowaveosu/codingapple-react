d7cf5f8e3c9e50ba95a405ceec6a6a05fcd574ff
nowaveosu <nowaveosu@gmail.com>
2023-05-02 20:17:26 +0900

    title changed

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@
 // 3. <Modal> </Modal> 혹은 <Modal/> 쓰기
 // 단, 다른 함수에 있던 변수는 컴포넌트에서 쓸수없음
 
-// * 동적인 UI 만드는 스텝
+// * 동적인 UI 만드는 스텝 (중요)
 // 1. html css로 미리 디자인 완성
 // 2. ui의 현재상태를 state로 저장
 // 3. state에 따라 ui 가 어떻게 보일지 작성
@@ -54,6 +54,7 @@ function App() {
   let [글제목, set글제목] = useState(['남자 코트 추천','강남우동추천','강의추천']);
   let [따봉, set따봉] = useState([0,0,0]);
   let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
 
   [1,2,3].map(function(a){
     console.log(a)
@@ -70,7 +71,7 @@ function App() {
         글제목.map((el,i) => {
           return(
             <div className='list' key={i} i = {i}>
-              <h4 onClick={() => {setModal(true)}}>{ 글제목[i] } 
+              <h4 onClick={() => {setModal(true); setTitle(i)}}>{ 글제목[i] } 
               <span onClick={() => { 따봉변경(따봉+1) }}>👍</span> {따봉}</h4>
               <p>2월 18일 발행</p>
             </div>
@@ -78,7 +79,7 @@ function App() {
         })
       }
       {
-        modal ? <Modal color = 'whitesmoke' 글제목={글제목} set글제목={set글제목}/> : null
+        modal ? <Modal color = 'whitesmoke' title = {title} 글제목={글제목} set글제목={set글제목}/> : null
       }
     </div>
   );
@@ -87,7 +88,7 @@ function App() {
 function Modal(props){
   return (
     <div className ="modal" style={{background: props.color}}>
-      <h4>{props.글제목[2]}</h4>
+      <h4>{props.글제목[props.title]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
       <button>글수정</button>
